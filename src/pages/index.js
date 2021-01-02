7031e07f491c54bfb08e490935e0437f476f9b8f
Rachelle Mariano <rachelle.mariano@gmail.com>
2021-01-02 14:39:32 -0500

    oh look its me

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,69 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 import { css } from "@emotion/core"
+import { withTheme } from "emotion-theming"
 
 import Layout from "../components/Layout"
 import Metadata from "../components/Metadata"
 import AboutMeBlurb from "../content/about.mdx"
 
-const IndexPage = () => (
-  <Layout>
-    <Metadata title="Home" description="Home page of rmariano.dev" />
-    <div
-      css={css`
-        display: flex;
-        justify-content: center;
-      `}
-    >
-      <div
-        css={css`
-          display: flex;
-          width: 50%;
-          flex-direction: column;
-        `}
-      >
-        <AboutMeBlurb />
-      </div>
-    </div>
-  </Layout>
-)
+const myPhotoQuery = graphql`
+  query {
+    me: file(relativePath: { eq: "general/me.jpg" }) {
+      ...fluidPhoto
+    }
+  }
+`
+
+const WrapBox = withTheme((props) => (
+  <div
+    css={css`
+      display: flex;
+      justify-content: center;
+      flex-wrap: ${props.theme.isMobile ? "wrap" : "nowrap"};
+    `}
+  >
+    {props.children}
+  </div>
+))
+
+const IndexPage = (props) => {
+  const imageQuery = useStaticQuery(myPhotoQuery)
+
+  return (
+    <Layout>
+      <Metadata title="Home" description="Home page of rmariano.dev" />
+      <WrapBox>
+        <div
+          css={css`
+            display: flex;
+            width: 100%;
+            flex-direction: column;
+            margin: 2vw;
+          `}
+        >
+          <AboutMeBlurb />
+        </div>
+        <div
+          css={css`
+            width: 100%;
+          `}
+        >
+          <Img
+            css={css`
+              height: 100%;
+            `}
+            alt="It's-a-me!"
+            fluid={imageQuery["me"].childImageSharp.fluid}
+            imgStyle={{
+              objectFit: "contain",
+            }}
+          />
+        </div>
+      </WrapBox>
+    </Layout>
+  )
+}
 
 export default IndexPage
